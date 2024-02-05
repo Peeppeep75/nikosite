42c213219528ac58138a002efb01448c1910efc4
Peeppeep75 <n_n_bsk.75@icloud.com>
2024-02-05 17:18:21 +0900

    ボタンの追加、画像の変更

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 "use steict";
+
+function searchByNickname() {
+  const birthdate = document.getElementById("birthdate").value;
+  const gender = document.querySelector('input[name="gender"]:checked');
+  const nickname = document.getElementById("nickname").value;
+}
