8161347e60ef50e8e07cdfa94e4fb68a0a19bc3e
Peeppeep75 <n_n_bsk.75@icloud.com>
2024-02-07 16:22:16 +0900

    生年月日と性別からのページ設定を始める

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
 "use steict";
 
-function searchByNickname() {
-  const birthdate = document.getElementById("birthdate").value;
-  const gender = document.querySelector('input[name="gender"]:checked');
-  const nickname = document.getElementById("nickname").value;
+function checkPersonality() {
+  const birthdate = document.getElementById("birthdate");
+  const gender = document.getSelection("gender");
 }
+
+let inputNumber;
+if (year === 1950) {
+  switch (month) {
+    case 1:
+      inputNumber = 32;
+      break;
+    case 2:
+      inputNumber = 3;
+      break;
+    case 3:
+      inputNumber = 31;
+      break;
+    case 4:
+      inputNumber = 2;
+      break;
+    case 5:
+      inputNumber = 32;
+      break;
+    case 6:
+      inputNumberr = 3;
+      break;
+    case 7:
+      inputNumber = 33;
+      break;
+    case 8:
+      inputNumber = 4;
+      break;
+    case 9:
+      inputNumber = 35;
+      break;
+    case 10:
+      inputNumber = 5;
+      break;
+    case 11:
+      inputNumber = 36;
+      break;
+    case 12:
+      inputNumber = 6;
+      break;
+  }
+} else if (year === 1951) {
+  //ここに追加で書いていくのかな・・・？
+}
+
+const day = birthdate();
+Number += day;
+
+if (Number >= 60) {
+  Number -= 60;
+}
+
+//男女によってページ表記を変える
